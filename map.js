f31a41d23eca64bdd9027b1e2067e2726c798640
tanudawande04 <tanudawande54@gmail.com>
2025-11-19 23:49:53 +0530

    added [ractice set files

--- map.js
+++ map.js
@@ -1,10 +1,45 @@
 //Implement your own .map() function....
 
 
-const array = [1, 2, 3, 4];
+// const array = [1, 2, 3, 4];
 
-const doubled = array.map(arr=> arr * 2);
+// const doubled = array.map(arr=> arr * 2);
 
-console.log(doubled); // [2, 4, 6, 8]
+// console.log(doubled); // [2, 4, 6, 8]
 
 
+
+
+Array.prototype.myMap = function (callback, thisArg) {
+    let result = [];
+
+    for (let i = 0; i < this.length; i++) {
+        // thisArg is used as "this" inside callback
+        result.push(callback.call(thisArg, this[i], i, this));
+    }
+
+    return result;
+};
+
+
+//1️⃣ Implement your own .map()
+
+const num = [1, 3, 4, 8, 9]
+let ans = num.map(n=> n * 2)
+console.log(ans);
+
+
+//filters..
+const arr = [1,2,3,4,5,6,7,8,9,0]
+const  sol = arr.filter(n => n%2 == 0)
+console.log(sol);
+
+//reduce..
+const nusm = [2,3,5,6]
+const as = nusm.reduce((acum ,curr) => {
+    return acum+ curr;
+    
+
+},0)
+console.log(as);
+    
