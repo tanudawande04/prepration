3abb9a7777dc3b4c49285d1e1c6f889fe4932237
tanudawande04 <tanudawande54@gmail.com>
2025-11-22 02:33:18 +0530

    added promises

--- destructuring.js
+++ destructuring.js
@@ -1,8 +1,24 @@
-const user = {name: "tanu" , age: 22}
-const {name , age} = user;
+const user = { name: "tanu", age: 22 }
+const { name, age } = user;
 console.log(name);
 
 
-const arr = [2,3,4,5,6,7,8]
-let ans = [...arr,9]
+const arr = [2, 3, 4, 5, 6, 7, 8]
+let ans = [...arr, 9]
 console.log(ans);
+
+
+async function getpost() {
+    try {
+        const res = await fetch();
+        const data = res.json("https://jsonplaceholder.typicode.com/posts/1")
+        console.log(data);
+
+
+    } catch (error) {
+        console.log("this is eror", error);
+
+    }
+}
+
+getpost()
